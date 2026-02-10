dc8f41e061b7d39d28a409b4ffd134d788c0db03
hhong98 <hh98@beancorp.com>
2026-02-10 12:58:18 +0900

    OptionComboBox 컨트롤 구현

---
+++ SmartFactoryMonitor/Controls/OptionComboBox.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartFactoryMonitor.Controls
+{
+    /// <summary>
+    /// Interaction logic for DropDownToggle.xaml
+    /// </summary>
+    public partial class OptionComboBox : UserControl
+    {
+        public OptionComboBox()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register(
+                "ItemsSource",
+                typeof(IEnumerable<string>),
+                typeof(OptionComboBox));
+
+        public IEnumerable<string> ItemsSource
+        {
+            get => (IEnumerable<string>)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedOptionProperty =
+            DependencyProperty.Register(
+                "SelectedOption",
+                typeof(string),
+                typeof(OptionComboBox),
+                new PropertyMetadata(null));
+
+        public object SelectedOption
+        {
+            get => (string)GetValue(SelectedOptionProperty);
+            set => SetValue(SelectedOptionProperty, value);
+        }
+
+        public static readonly DependencyProperty PlaceholderProperty =
+             DependencyProperty.Register(
+                 "Placeholder",
+                 typeof(string),
+                 typeof(OptionComboBox),
+                 new PropertyMetadata("Placeholder"));
+
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+
+        private void OnOptionClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                SelectedOption = button.Content.ToString();
+                FilterToggle.IsChecked = false;
+            }
+        }
+    }
+}
