1a3665807a66dfa4b3ac92355d7d18e989f5b47a
hhong98 <hh98@beancorp.com>
2026-02-13 11:27:42 +0900

    설비 모니터링 가동 설비 대시보드 메시지 수정

---
+++ SmartFactoryMonitor/Common/TotalDashboardMsgConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SmartFactoryMonitor.Common
+{
+    public class TotalDashboardMsgConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values is null || values.Length < 3) return "";
+
+            return values[0] is bool isPanelOpened &&
+                values[1] is int activeCount &&
+                values[2] is int totalCount
+                ? $"{activeCount}"
+                //? isPanelOpened ? $"{activeCount}" : $"{activeCount}/{totalCount}"
+                : "-";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
